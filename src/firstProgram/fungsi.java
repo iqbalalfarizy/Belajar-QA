f33c819b8ee3fc94921c5a1049f83e30cb4a5932
iqbalalfarizy <iqbalalfarizy06@gmail.com>
2023-10-17 16:57:34 +0700

    commit pertama

---
+++ src/firstProgram/fungsi.java
@@ -0,0 +1,22 @@
+package firstProgram;
+
+import static firstProgram.FirstProgram.nama;
+
+public class fungsi {
+    public static void  main(String[] args){
+        int x = 6;
+        int y = 12;
+        int z = 16;
+
+        nama("iqbal");
+        jumlah (x, y);
+        jumlah (y, z);
+    }
+
+
+    public static int jumlah (int a, int b){
+        int hasil = a + b;
+        System.out.println("Hasil penjumlahan "+a+" dan "+b+" adalah " + hasil);
+        return hasil;
+    }
+}
